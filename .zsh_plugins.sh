51e3d11bd58e5787c3a9d06218643f461edd89cb
adux <adriangarate@protonmail.com>
2023-06-29 12:43:19 +0200

    zsh files

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -1,1 +1,8 @@
-/home/adux/.zsh_plugins.sh
+source /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-subnixr-SLASH-minimal/minimal.zsh
+fpath+=( /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-subnixr-SLASH-minimal )
+source /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/adux/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
